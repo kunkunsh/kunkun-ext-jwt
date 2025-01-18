664b37b8de58c189d34f920e3cf2dd4fdce8d854
Huakun Shen <huakun.shen@huakunshen.com>
2025-01-18 03:42:00 -0500

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
+	import { ui } from '@kksh/api/ui/iframe';
+	import { updateTheme } from '@kksh/svelte';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		ui.registerDragRegion();
+		ui.getTheme().then((theme) => {
+			updateTheme(theme);
+		});
+	});
+</script>
+
+<ModeWatcher />
+<slot></slot>
